b89efaf77442214cfb23fcf7254a99ad5637e74f
181220075-周其乐 <qile0402@163.com>
2019-12-18 23:44:38 +0800

    p1 using 128

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,6 +1,22 @@
 #include "multimod.h"
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-  int64_t ans = ((__int128)(a * b) % m);
-  return ans;
+  
+  int64_t ret = 0;
+  
+  __int128 alarge=(__int128)a;
+  __int128 blarge=(__int128)b;
+
+  while(blarge) {
+
+    if(blarge&1){
+      ret = (ret + alarge) % m;
+    }
+
+    alarge<<= 1;
+    alarge=alarge% m;
+    blarge>>= 1;
+  }
+  return ret;
+  
 }
--- multimod/test.py
+++ multimod/test.py
@@ -11,11 +11,17 @@ for i in range(1000):
     a=random.randint(0,LIMIT)
     b=random.randint(0,LIMIT)
     m=random.randint(1,LIMIT)
-    whole_command ="/home/qile/ics-workbench/multimod/main -i "+choosen_func_no+str(a)+" "+str(b)+" "+str(m)
+
+    whole_command ="/home/qile/ics-workbench/multimod/main -i "+\
+                   choosen_func_no+str(a)+" "+str(b)+" "+str(m)
     opened_file=os.popen(whole_command)
     my_result=opened_file.readlines()[0].strip()
-    if(int(my_result)!=a*b%m):
-        wrong_num=wrong_num+1
-        print("wrong result: "+str(a)+"*"+str(b)+"%"+str(m)+"my_result:"+str(my_result)+" right result:"+str(a*b%m)+"\n")
     
-print("in 1000 tests, wrong times are:"+str(wrong_num)+"\n")
+    if(int(my_result)!=(a*b%m)):
+        wrong_num=wrong_num+1
+        print("wrong result: "+"\n"\
+              +str(a)+" * "+str(b)+" % "+str(m)+"\n"\
+              +"my_result:"+str(my_result)+"\n"\
+              +"right result:"+str(a*b%m)+"\n")
+    
+print("in 1000 tests, right times are:"+str(1000-wrong_num)+ " wrong times are:"+str(wrong_num)+"\n")
