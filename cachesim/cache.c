af41b955a12f104711b84d1ede47d33b0e499f56
tracer-nju <tracer@nju.edu.cn>
2019-12-26 11:59:03 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: acaf5baf55eb4729bd759a518188812c            Boot ID: 7a1abcd79c1943fcba58cc3460d4db11     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  11:59:03 up 23:35,  1 user,  load average: 0.29, 0.13, 0.11

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -115,9 +115,9 @@ void write_data(uintptr_t addr, uint32_t group_label, uint32_t line, uint32_t da
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   printf("cache_write\n");
   uint32_t cache_group_label= (addr>>mm_block_addr_bit)%mm_group_label_bit;
-  printf("group_label: %b\n",cache_group_label);
+  printf("group_label: %d\n",cache_group_label);
   uint32_t mm_tag = addr >> (mm_block_addr_bit+mm_group_label_bit);
-  printf("mm_tag: %b\n",mm_tag);
+  printf("mm_tag: %d\n",mm_tag);
   uint32_t find_hit_line = find_hit_tag(addr,cache_group_label,mm_tag);
   //命中
   if(find_hit_line>=0){
